a42c85cf01a10e5e8c4ec5099a74d337797623ea
Haneul Lee <lovesky4294@gmail.com>
2019-09-14 21:45:35 +0900

    delete, getMyPlaces resolver

---
+++ src/api/Place/GetMyPlaces/GetMyPlaces.resolvers.ts
@@ -0,0 +1,42 @@
+
+import User from "../../../entities/User";
+import { GetMyPlacesResponse } from "../../../types/graph";
+import { Resolvers } from "../../../types/resolvers";
+import privateResolver from "../../../utils/privateResolver";
+
+const resolvers: Resolvers = {
+    Query: {
+        GetMyPlaces: privateResolver(
+            async (_, __, { req }): Promise<GetMyPlacesResponse> => {
+                try {
+                    const user = await User.findOne(
+                        { id: req.user.id },
+                        { relations: ["places"] }
+                    );
+                    if (user) {
+                        console.log(user.places);
+                        return {
+                            ok: true,
+                            error: null,
+                            places: user.places
+                        };
+                    } else {
+                        return {
+                            ok: false,
+                            error: "User not found",
+                            places: null
+                        };
+
+                    }
+                } catch (error) {
+                    return {
+                        ok: false,
+                        error: error.message,
+                        places: null
+                    };
+                }
+            }
+        )
+    }
+};
+export default resolvers;
